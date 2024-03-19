beea4cdea80e8dc4268ba91d6c0129fde78e3f03
EnesOzturk4 <ozturkenes614@gmail.com>
2024-03-19 14:00:24 +0300

    b

--- MVCGrup2/Migrations/20240319105542_mig1.Designer.cs
+++ MVCGrup2/Migrations/20240319105542_mig1.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVCGrup2.Migrations
 {
     [DbContext(typeof(MVCGrup2Context))]
-    [Migration("20240319102421_m1")]
-    partial class m1
+    [Migration("20240319105542_mig1")]
+    partial class mig1
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- MVCGrup2/Migrations/20240319105542_mig1.cs
+++ MVCGrup2/Migrations/20240319105542_mig1.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace MVCGrup2.Migrations
 {
     /// <inheritdoc />
-    public partial class m1 : Migration
+    public partial class mig1 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
