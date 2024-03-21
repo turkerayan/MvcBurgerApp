ac20c4f9ac0173f04150d7bc053fd526ec442205
EnesOzturk4 <ozturkenes614@gmail.com>
2024-03-21 12:07:00 +0300

    mvcburger

---
+++ MVCGrup2/Areas/Admin/MenuController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCGrup2.Areas.Admin
+{
+    public class MenuController:Controller
+    {
+
+
+
+    }
+}
---
+++ MVCGrup2/Models/MenuModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCGrup2.Models
+{
+    public class MenuModel
+    {
+        [Required(ErrorMessage = "Menü adı boş olamaz.")]
+        public string Ad { get; set; }
+        [Required(ErrorMessage = "Menü fiyatı boş olamaz.")]
+        public decimal? Fiyat { get; set; }
+        public IFormFile Resim
+        {
+            get; set;
+
+        }
+    }
+}
