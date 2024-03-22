bf5e63ac20934edacc3e957b88c594ae9addc478
halitermet <halitermet3333@gmail.com>
2024-03-22 15:05:46 +0300

    edit

--- MVCGrup2/Program.cs
+++ MVCGrup2/Program.cs
@@ -1,51 +1,80 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MVCGrup2.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MVCGrup2.Data;
+
 namespace MVCGrup2
+
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
-   var connectionString = builder.Configuration.GetConnectionString("MVCGrup2ContextConnection") ?? throw new InvalidOperationException("Connection string 'MVCGrup2ContextConnection' not found.");
 
-   builder.Services.AddDbContext<MVCGrup2Context>(options => options.UseSqlServer(connectionString));
+    public class Program
 
-   builder.Services.AddDefaultIdentity<MVCGrup2User>(options => options.SignIn.RequireConfirmedAccount = true)
-				.AddRoles<IdentityRole>()
-				.AddEntityFrameworkStores<MVCGrup2Context>();
+    {
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+        public static void Main(string[] args)
 
-			var app = builder.Build();
+        {
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+            var builder = WebApplication.CreateBuilder(args);
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            var connectionString = builder.Configuration.GetConnectionString("MVCGrup2ContextConnection") ?? throw new InvalidOperationException("Connection string 'MVCGrup2ContextConnection' not found.");
 
-			app.UseRouting();
+            builder.Services.AddDbContext<MVCGrup2Context>(options => options.UseSqlServer(connectionString));
 
-			app.UseAuthentication();
+            builder.Services.AddDefaultIdentity<MVCGrup2User>(options => options.SignIn.RequireConfirmedAccount = true)
 
-			app.UseAuthorization();
+                         .AddRoles<IdentityRole>()
 
-			app.MapRazorPages();
+                         .AddEntityFrameworkStores<MVCGrup2Context>();
+
+            // Add services to the container.
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            if (!app.Environment.IsDevelopment())
+
+            {
+
+                app.UseExceptionHandler("/Home/Error");
+
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
+                app.UseHsts();
+
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
 
 
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
-         
+            app.MapControllerRoute(
+
+                name: "default",
+
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
             app.Run();
-		}
-	}
+
+        }
+
+    }
+
 }
+
+
+
